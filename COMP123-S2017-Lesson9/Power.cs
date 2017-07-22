c43cf88958c979248ec6e51a108fd63ef97e506d
Imba-lol <best.abuka@gmail.com>
2017-07-22 16:20:02 -0400

    Added Power Class

---
+++ COMP123-S2017-Lesson9/Power.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/* 
+ * Abubakir Myrzaly
+ * Date: July 11,2017
+ * Description: This is Power Class
+ * Version: 0.1 - Created the Power class
+ */
+
+namespace COMP123_S2017_Lesson9
+{
+    /// <summary>
+    /// This is Power Class chich will be use as a Data type
+    /// </summary>
+    public class Power
+    {
+        // PUBLIC PROPERTIES
+        
+        public string Name { get; set; }
+
+        public int Rank { get; set; }
+        // CONSTRUCTORS ----------
+
+    }
+}
